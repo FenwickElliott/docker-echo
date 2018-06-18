6aa4b6bc5edda60f39532c448d8a65449969e500
Charles Fenwick Elliott <charles@fenwickelliott.io>
2018-06-18 12:53:20 -0400

    importing satori/go.uuid

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 # build stage
-FROM golang:alpine AS build-env
+FROM iron/go:dev AS build-env
 ADD . /src
+RUN go get -u github.com/satori/go.uuid
 RUN cd /src && go build -o main
 
 # final stage
 FROM alpine
 WORKDIR /app
-COPY --from=build-env /src/main /app/
-ENTRYPOINT ./main
+COPY --from=build-env /src/ /app/
+ENTRYPOINT ["./main"]
--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/satori/go.uuid"
 )
 
 func main() {
 	http.HandleFunc("/", write)
+	http.HandleFunc("/newID", newID)
 	fmt.Println("listening on: 9090")
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
@@ -16,4 +19,11 @@ func main() {
 func write(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL)
 	io.WriteString(w, r.URL.Path+"\n")
+}
+
+func newID(w http.ResponseWriter, r *http.Request) {
+	newID := uuid.Must(uuid.NewV4())
+
+	fmt.Println(newID)
+	io.WriteString(w, newID.String()+"\n")
 }
